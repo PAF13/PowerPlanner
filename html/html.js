e5578e05ce1afda557fe57d2b69af9025d3a7cfd
Patrick Favors <antoniofavors@gmail.com>
2020-12-25 12:45:19 +0100

    Update

--- html/html.js
+++ html/html.js
@@ -1,3 +1,4 @@
+let menuOpen = 0;
 function overmenu(x) {
   x.style.backgroundColor = "black";
   console.log(x)
@@ -5,4 +6,16 @@ function overmenu(x) {
 
 function outmenu(x) {
   x.style.backgroundColor = "teal";
+}
+
+function openmenu(x) {
+  document.getElementById("console").style.transition = "0.7s";
+  if(menuOpen == 0) {
+    document.getElementById("console").style.left = "200px";
+    menuOpen = 1;
+  }
+  else {
+    document.getElementById("console").style.left = "0px";
+    menuOpen = 0;
+  }
 }
