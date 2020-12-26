54c8814cd509766055c203163efc01ca3418c598
Patrick Favors <antoniofavors@gmail.com>
2020-12-26 14:31:40 +0100

    Update

--- html/js/html.js
+++ html/js/html.js
@@ -3,11 +3,11 @@ $(function(){
   //onclick for menu
   $(".console div i").on('click', function() {
     if (menuOpen == 0) {
-      $(".console").css({"left":"150px", "border-radius": "10px 0px 0px 10px", "transition":"0.5s"});
+      $(".console").css({"left":"150px","right":"-150px", "border-radius": "10px 0px 0px 10px", "transition":"0.5s"});
       menuOpen = 1;
     }
     else {
-      $(".console").css({"left":"0px","border-radius": "0px 0px 0px 0px", "transition":"0.5s"});
+      $(".console").css({"left":"0px","right":"0px","border-radius": "0px 0px 0px 0px", "transition":"0.5s"});
       menuOpen = 0;
     }
   });
