0757af578405d649f5049f273dae6a9fe73ea1a5
Patrick Favors <antoniofavors@gmail.com>
2020-12-26 13:43:57 +0100

    Update

--- html/js/html.js
+++ html/js/html.js
@@ -1,4 +1,19 @@
+let menuOpen = 0;
 $(function(){
+  //onclick for menu
+  $(".console div i").on('click', function() {
+    if (menuOpen == 0) {
+      $(".console").css({"left":"150px", "border-radius": "10px 0px 0px 10px", "transition":"0.5s"});
+      menuOpen = 1;
+    }
+    else {
+      $(".console").css({"left":"0px","border-radius": "0px 0px 0px 0px", "transition":"0.5s"});
+      menuOpen = 0;
+    }
+  });
 
-  $("#console").hide(500);
+  $(".menu div").on('click', function() {
+    $(".console div span").html($(this).html());
+  });
+
 });
